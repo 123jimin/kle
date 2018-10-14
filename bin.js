5f5a70da3448b5467a65f70b27f556c245e8f5b1
Jimin Park <pjm950@gmail.com>
2018-10-14 22:26:07 +0900

    Fixed a bug on applying patches to zooms, and added a basic import system.

--- bin.js
+++ bin.js
@@ -1,14 +1,14 @@
 // KLE - kshoot lane effector
 
 const fs = require('fs');
-const KLEProcessor = require("./processor.js");
+const KLEProcessor = require("./lib/processor.js");
 
 const file_names = process.argv.slice(-3);
 const in_file_name = file_names[0];
 const kle_file_name = file_names[1];
 const out_file_name = file_names[2];
 
-const proc = new KLEProcessor(fs.readFileSync(kle_file_name, 'utf-8'));
+const proc = new KLEProcessor(kle_file_name);
 const result = proc.process(fs.readFileSync(in_file_name, 'utf-8'));
 
 fs.writeFileSync(out_file_name, result, 'utf-8');
